e0ab68337627bbe3ac262cc1a54c60aab9972c82
Marek Nowak <mareknowakk@gmail.com>
2016-11-08 11:20:45 +0100

    Implements choosing the coolest animal.

--- src/coolest_animal/core.clj
+++ src/coolest_animal/core.clj
@@ -1,7 +1,19 @@
-(ns coolest-animal.core
-  (:gen-class))
+(ns coolest-animal.core (:require [digest :refer [md5]]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn- cool-factor [animal] (->> animal
+                                 md5
+                                 char-array
+                                 (map int)
+                                 (reduce +)))
+
+(defn- coolest [animals] (->> animals
+                              (sort-by cool-factor)
+                              last))
+
+(defn -main "Reads a supplied list of animals and chooses the coolest one." []
+  (let [input-directory (System/getenv "INPUT1_STAGING_DIR")
+        input-file (clojure.java.io/file input-directory "animals.txt")
+        output-directory (System/getenv "OUTPUT1_STAGING_DIR")
+        output-file (clojure.java.io/file output-directory "coolest-animal.txt")
+        animals (-> input-file slurp (clojure.string/split #"\n"))]
+    (spit output-file (coolest animals))))
